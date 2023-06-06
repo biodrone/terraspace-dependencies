f5653dcb574e463380c98dd6a196eed4038fb603
Joshua Jacobs <joshua.jacobs@netcompany.com>
2023-06-06 10:53:27 +0100

    fix: version pin terraform install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install Terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install terraform
+sudo yum -y install terraform-1.4.6-1.x86_64
 
 # Install Terraspace Gem Dependencies
 gem install azure_info-0.1.5.gem
