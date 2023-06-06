32228931cdc3b1847236774543295cd10c430cfa
Joshua Jacobs <joshua.jacobs@netcompany.com>
2023-06-06 10:40:57 +0100

    fix: installer permissions

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashi
 sudo yum -y install terraform
 
 # Install Terraspace Gem Dependencies
-gem install azure_info-0.1.5.gem
-gem install armrest-0.1.3.gem
+sudo gem install azure_info-0.1.5.gem
+sudo gem install armrest-0.1.3.gem
 
 # Install Terraspace
-rpm -ivh terraspace-latest.rpm
+sudo rpm -ivh terraspace-latest.rpm
 
 # Version Checks
 terraform version
