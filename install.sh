5f800756403d0efa5cd15c09b2972f0983f3e6c1
Joshua Jacobs <joshua.jacobs@netcompany.com>
2023-06-06 10:30:17 +0100

    fix: add all tools to installer

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Install Terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+
+# Install Terraspace Gem Dependencies
 gem install azure_info-0.1.5.gem
 gem install armrest-0.1.3.gem
 
+# Install Terraspace
+rpm -ivh terraspace-latest.rpm
+
+# Version Checks
+terraform version
+terraspace version
